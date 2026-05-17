e392d10f7f919804a3c4dd63e2d9bfe08ba49836
Odoom21 <ananemaxwellodoom@gmail.com>
2026-05-17 23:37:03 +0000

    Fix typescript error in canvas-confetti shapeFromPath

--- components/sections/FinaleScene.tsx
+++ components/sections/FinaleScene.tsx
@@ -43,6 +43,7 @@ export default function FinaleScene() {
       const scalar = 2;
       const heart = confetti.shapeFromPath({
         path: 'M167 72c19,-38 37,-56 75,-56 42,0 76,33 76,75 0,76 -76,151 -151,227 -76,-76 -151,-151 -151,-227 0,-42 33,-75 75,-75 38,0 57,18 76,56z',
+        // @ts-ignore: canvas-confetti accepts an array at runtime but types expect DOMMatrix
         matrix: [0.03333333333333333, 0, 0, 0.03333333333333333, -5.566666666666666, -5.533333333333333]
       });
 
